56f27fae5364cbf5fdf341482b5b015e239f3425
KaziBadrul <kazibadrul03@gmail.com>
2026-03-02 18:34:15 +0600

    fix deployment 2

--- acadex/next.config.ts
+++ acadex/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@tiptap/extension-bubble-menu", "@tiptap/extension-floating-menu", "@tiptap/react", "@floating-ui/dom", "@floating-ui/core", "react-force-graph-2d", "force-graph", "react-kapsule"],
+  transpilePackages: [
+    "lucide-react",
+    "@tiptap/extension-bubble-menu",
+    "@tiptap/extension-floating-menu",
+    "@tiptap/react",
+    "@floating-ui/dom",
+    "@floating-ui/core",
+    "react-force-graph-2d",
+    "force-graph",
+    "react-kapsule"
+  ],
   serverExternalPackages: [],
 };
 
